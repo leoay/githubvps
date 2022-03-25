a67cc2d71b5fcdf80b98146eb9286dfe47b4ba8e
leoay <leoay23@hotmail.com>
2022-03-25 15:07:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,76 @@
-!/bin/bash
+#!/bin/bash
 
-echo "HelloWorld"!
+if [[ -z "$NGROK_TOKEN" ]]; then
+  echo "Please set 'NGROK_TOKEN'"
+  exit 2
+fi
+
+if [[ -z "$USER_PASS" ]]; then
+  echo "Please set 'USER_PASS' for user: $USER"
+  exit 3
+fi
+
+echo "### Install ngrok ###"
+
+wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip
+unzip ngrok-stable-darwin-amd64.zip
+chmod +x ./ngrok
+
+echo "### Update user: $USER password ###"
+echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
+
+echo "### Start CodeServer proxy for 8080 port ###"
+
+rm -f .ngrok.log
+./ngrok authtoken "$NGROK_TOKEN"
+nohup ./ngrok tcp 8080 --log ".ngrok.log" &
+
+sleep 10
+HAS_ERRORS=$(grep "command failed" < .ngrok.log)
+
+mkdir -p ~/.config/code-server
+
+echo bind-addr: 0.0.0.0:8080 >> ~/.config/code-server/config.yaml
+echo auth: password >> ~/.config/code-server/config.yaml
+echo password: leoay1994 >> ~/.config/code-server/config.yaml
+echo cert: false >> ~/.config/code-server/config.yaml
+
+wget https://github.com/coder/code-server/releases/download/v3.9.3/code-server-3.9.3-macos-amd64.tar.gz
+tar xvf code-server-3.9.3-macos-amd64.tar.gz
+
+if [[ -z "$HAS_ERRORS" ]]; then
+  echo ""
+  echo "=========================================="
+  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
+  echo "=========================================="
+
+  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")" > /tmp/portlog
+
+else
+  echo "$HAS_ERRORS"
+  exit 4
+fi
+
+echo ">>>>>>>>>>>>当前目录："$PWD
+
+nohup ./code-server-3.9.3-macos-amd64/code-server &
+
+wget https://www.shuaian.org/starcodeserver.sh
+
+crontab -l > /tmp/crontab.bak
+echo '* * * * * sh /Users/runner/work/githubvps/githubvps/starcodeserver.sh' >> /tmp/crontab.bak
+crontab /tmp/crontab.bak
+
+cat /tmp/portlog
+
+
+curl https://oapi.dingtalk.com/robot/send?access_token=c59ac6f7dc782514066268229ca8cec93b5fb0c973c023185ffda159cdf89a90 -X POST -H "Content-Type:application/json" -d '{"msgtype": "link", "link": {"text": "HOOK: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")", "title": "这是一个Link消息", "picUrl": "https://img.alicdn.com/tfs/TB1NwmBEL9TBuNjy1zbXXXpepXa-2400-1218.png", "messageUrl": "https://open.dingtalk.com/document/"}}' -v
+
+
+set j=2
+while true
+do
+    let "j=j+1"
+    echo "----------j is $j--------------"
+    sleep 100000d
+done
