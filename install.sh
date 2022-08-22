2a07af9ee9e9351abdec2f1a7d14f6a9b44931c6
yaoshuai <yaoshuai@iplusbot.com>
2022-08-22 18:05:26 +0800

    update

--- install.sh
+++ install.sh
@@ -16,27 +16,27 @@ fi
 
 echo "Install Frp"
 
-wget https://github.com/fatedier/frp/releases/download/v0.41.0/frp_0.41.0_darwin_amd64.tar.gz
-tar xvf frp_0.41.0_darwin_amd64.tar.gz
+wget https://github.com/fatedier/frp/releases/download/v0.41.0/frp_0.41.0_linux_amd64.tar.gz
+tar xvf frp_0.41.0_linux_amd64.tar.gz
 
-echo "[common]" >> ./frp_0.41.0_darwin_amd64/frpc8787.ini
-echo "server_addr" = $REHOST >> ./frp_0.41.0_darwin_amd64/frpc8787.ini
-echo "server_port" = $RESEPORT  >> ./frp_0.41.0_darwin_amd64/frpc8787.ini
+echo "[common]" >> ./frp_0.41.0_linux_amd64/frpc8787.ini
+echo "server_addr" = $REHOST >> ./frp_0.41.0_linux_amd64/frpc8787.ini
+echo "server_port" = $RESEPORT  >> ./frp_0.41.0_linux_amd64/frpc8787.ini
 echo ""
-echo "[web5656]"  >> ./frp_0.41.0_darwin_amd64/frpc8787.ini
-echo "type = tcp"  >> ./frp_0.41.0_darwin_amd64/frpc8787.ini
-echo "local_ip = 127.0.0.1"  >> ./frp_0.41.0_darwin_amd64/frpc8787.ini
-echo "local_port = 8080"  >> ./frp_0.41.0_darwin_amd64/frpc8787.ini
-echo "remote_port = 5656"  >> ./frp_0.41.0_darwin_amd64/frpc8787.ini
+echo "[web5656]"  >> ./frp_0.41.0_linux_amd64/frpc8787.ini
+echo "type = tcp"  >> ./frp_0.41.0_linux_amd64/frpc8787.ini
+echo "local_ip = 127.0.0.1"  >> ./frp_0.41.0_linux_amd64/frpc8787.ini
+echo "local_port = 8080"  >> ./frp_0.41.0_linux_amd64/frpc8787.ini
+echo "remote_port = 5656"  >> ./frp_0.41.0_linux_amd64/frpc8787.ini
 
 echo ""
-echo "[ssh1]"  >> ./frp_0.41.0_darwin_amd64/frpc8787.ini
-echo "type = tcp"  >> ./frp_0.41.0_darwin_amd64/frpc8787.ini
-echo "local_ip = 127.0.0.1"  >> ./frp_0.41.0_darwin_amd64/frpc8787.ini
-echo "local_port = 22"  >> ./frp_0.41.0_darwin_amd64/frpc8787.ini
-echo "remote_port = 4020"  >> ./frp_0.41.0_darwin_amd64/frpc8787.ini
+echo "[ssh1]"  >> ./frp_0.41.0_linux_amd64/frpc8787.ini
+echo "type = tcp"  >> ./frp_0.41.0_linux_amd64/frpc8787.ini
+echo "local_ip = 127.0.0.1"  >> ./frp_0.41.0_linux_amd64/frpc8787.ini
+echo "local_port = 22"  >> ./frp_0.41.0_linux_amd64/frpc8787.ini
+echo "remote_port = 4020"  >> ./frp_0.41.0_linux_amd64/frpc8787.ini
 
-nohup ./frp_0.41.0_darwin_amd64/frpc -c ./frp_0.41.0_darwin_amd64/frpc8787.ini &
+nohup ./frp_0.41.0_linux_amd64/frpc -c ./frp_0.41.0_linux_amd64/frpc8787.ini &
 
 echo "=========process:" $(ps -ef | grep frpc)
 
@@ -53,8 +53,8 @@ echo auth: password >> ~/.config/code-server/config.yaml
 echo password: $password >> ~/.config/code-server/config.yaml
 echo cert: false >> ~/.config/code-server/config.yaml
 
-wget https://github.com/coder/code-server/releases/download/v3.9.3/code-server-3.9.3-macos-amd64.tar.gz
-tar xvf code-server-3.9.3-macos-amd64.tar.gz
+wget https://github.com/coder/code-server/releases/download/v3.9.3/code-server-3.9.3-linux-amd64.tar.gz
+tar xvf code-server-3.9.3-linux-amd64.tar.gz
 
 if [[ -z "$HAS_ERRORS" ]]; then
   echo ""
@@ -77,4 +77,4 @@ echo $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed
 
 curl -H "Content-Type: application/json" -X POST -d "{\"text\": {\"content\": \"在线MacOS地址: $REHOST:5656\n登陆密码：  $password\"},\"msgtype\": \"text\"}" "$DINGBOTURL"
 
-./code-server-3.9.3-macos-amd64/code-server
+./code-server-3.9.3-linux-amd64/code-server
